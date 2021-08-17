57e322d304509cbbc954d9f9b7de8e614885d452
Catherine Zhou <32141721+catherinecscst@users.noreply.github.com>
2021-08-16 22:09:26 -0400

    Add files via upload

---
+++ CSC418/A4__computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/AABBTree.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/AABBTree.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/AABBTree_ray_intersect.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/AABBTree_ray_intersect.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/box_box_intersect.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/box_box_intersect.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/find_all_intersecting_pairs_using_AABBTrees.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/find_all_intersecting_pairs_using_AABBTrees.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/insert_box_into_box.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/insert_box_into_box.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/insert_triangle_into_box.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/insert_triangle_into_box.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/nearest_neighbor_brute_force.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/nearest_neighbor_brute_force.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/point_AABBTree_squared_distance.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/point_AABBTree_squared_distance.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/point_box_squared_distance.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/point_box_squared_distance.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/ray_intersect_box.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/ray_intersect_box.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/ray_intersect_triangle.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/ray_intersect_triangle.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/ray_intersect_triangle_mesh_brute_force.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/ray_intersect_triangle_mesh_brute_force.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/src/triangle_triangle_intersection.cpp" "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/CMakeFiles/core.dir/src/triangle_triangle_intersection.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../libigl/cmake/../include"
+  "../libigl/cmake/../external/eigen"
+  "../libigl/external/glad/include"
+  "../libigl/external/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/h/u3/c5/04/zhouqua7/csc418/A4/computer-graphics-bounding-volume-hierarchy/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
