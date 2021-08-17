f9654dd204f6f8bcd4c4410cf82ef8c3b97c284a
Catherine Zhou <32141721+catherinecscst@users.noreply.github.com>
2021-08-16 21:56:40 -0400

    Delete src directory

--- src/reflect.cpp
+++
@@ -1,9 +0,0 @@
-#include <Eigen/Core>
-
-Eigen::Vector3d reflect(const Eigen::Vector3d & in, const Eigen::Vector3d & n)
-{
-
-  // Given an "incoming" vector and a normal vector, compute the mirror reflected "outgoing" vector.
-  return (in - 2 * (in.dot(n)) * n).normalized();
-  
-}
