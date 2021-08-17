e9c7b4ceb5ee143938c38a4a6885e4c9601904c7
Catherine Zhou <32141721+catherinecscst@users.noreply.github.com>
2021-08-16 21:33:49 -0400

    Add files via upload

---
+++ CSC418/A1__computer-graphics-raster-images/build/CMakeFiles/raster.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/main.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/main.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/demosaic.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/demosaic.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/desaturate.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/desaturate.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/hsv_to_rgb.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/hsv_to_rgb.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/hue_shift.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/hue_shift.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/over.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/over.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/reflect.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/reflect.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/rgb_to_gray.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/rgb_to_gray.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/rgb_to_hsv.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/rgb_to_hsv.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/rgba_to_rgb.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/rgba_to_rgb.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/rotate.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/rotate.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/simulate_bayer_mosaic.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/simulate_bayer_mosaic.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/src/write_ppm.cpp" "/h/u3/c5/04/zhouqua7/csc418/A1/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/write_ppm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../json"
+  "../shared/json"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
