0ae8065bb1310bb444efb2339082edc0e5f5e798
Catherine Zhou <32141721+catherinecscst@users.noreply.github.com>
2021-08-16 22:00:22 -0400

    Add files via upload

---
+++ CSC418/A3__computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/Plane.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/src/Plane.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/Sphere.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/src/Sphere.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/Triangle.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/src/Triangle.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/TriangleSoup.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/src/TriangleSoup.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/first_hit.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/src/first_hit.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/viewing_ray.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/src/viewing_ray.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/write_ppm.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/src/write_ppm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../eigen"
+  "../json"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CSC418/A3__computer-graphics-ray-tracing/build/CMakeFiles/raytracing.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/main.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/raytracing.dir/main.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/DirectionalLight.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/raytracing.dir/src/DirectionalLight.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/PointLight.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/raytracing.dir/src/PointLight.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/blinn_phong_shading.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/raytracing.dir/src/blinn_phong_shading.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/raycolor.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/raytracing.dir/src/raycolor.cpp.o"
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/src/reflect.cpp" "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/raytracing.dir/src/reflect.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../eigen"
+  "../json"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/h/u3/c5/04/zhouqua7/csc418/A3/computer-graphics-ray-tracing/build/CMakeFiles/hw2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
